a67cf11d6a90879656c1239a3b2894d36d454cbd
edsilv <e.silverton@gmail.com>
2018-07-10 15:00:59 +0100

    await iiiframe

--- examples/iiiframe.js
+++ examples/iiiframe.js
@@ -39,7 +39,7 @@ function getPaintingAnnotation(annos) {
     for (let i = 0; i < annos.length; i++) {
         const anno = annos[i];
         const motivation = anno.getMotivation().value;
-        if (motivation.toLowerCase() === "painting") {
+        if (motivation.toLowerCase() === 'painting') {
             return anno;
         }
     }
@@ -49,7 +49,7 @@ async function parseAnnotation(anno, entity) {
     
     const motivation = anno.getMotivation().value;
 
-    if (motivation.toLowerCase() === "painting") {
+    if (motivation.toLowerCase() === 'painting') {
         return;
     }
 
@@ -59,30 +59,30 @@ async function parseAnnotation(anno, entity) {
     json = JSON.parse(json);
 
     switch (motivation) {
-        case "scale" :
+        case 'scale' :
 
-            if (entity.nodeName === "A-IMAGE") {
+            if (entity.nodeName === 'A-IMAGE') {
                 // add width and height
-                const width = document.createAttribute("width");
+                const width = document.createAttribute('width');
                 width.value = json.x / scalefactor;
                 entity.setAttributeNode(width);
 
-                const height = document.createAttribute("height");
+                const height = document.createAttribute('height');
                 height.value = json.y / scalefactor;
                 entity.setAttributeNode(height);
             } else {
                 // add scale component
-                const scale = document.createAttribute("scale");
+                const scale = document.createAttribute('scale');
                 scale.value = `${json.x} ${json.y} ${json.z}`;
                 entity.setAttributeNode(scale);
             }
             
             break;
-        case "rotation" :
+        case 'rotation' :
             // add rotation component
             break;
-        case "position" :
-            const position = document.createAttribute("position");
+        case 'position' :
+            const position = document.createAttribute('position');
             position.value = `${json.x} ${json.y} ${json.z}`;
             entity.setAttributeNode(position);
             break;
@@ -96,28 +96,28 @@ function parsePaintingAnnotation(anno) {
     let entity;
     
     switch (format.toLowerCase()) {
-        case "image/jpeg" :
+        case 'image/jpeg' :
             entity = document.createElement('a-image');
 
-            // geometry component
-            // https://github.com/aframevr/aframe/blob/bbc2f0325cdd3c4bd95a69ce4ce9705b0e6a041d/src/extras/primitives/primitives/a-image.js
-            // const geometry = document.createAttribute("geometry");
-            // geometry.value = "primitive: plane;"
-            // entity.setAttributeNode(geometry);
-
             // src component
-            const src = document.createAttribute("src");
+            const src = document.createAttribute('src');
             src.value = body.id;
             entity.setAttributeNode(src);
 
+            // geometry component
+            // https://github.com/aframevr/aframe/blob/bbc2f0325cdd3c4bd95a69ce4ce9705b0e6a041d/src/extras/primitives/primitives/a-image.js
+            // const geometry = document.createAttribute('geometry');
+            // geometry.value = 'primitive: plane;'
+            // entity.setAttributeNode(geometry);
+
             // // material component
-            // const material = document.createAttribute("material");
-            // material.value = "color: #FFF; shader: flat; side: double; transparent: true";
+            // const material = document.createAttribute('material');
+            // material.value = 'color: #FFF; shader: flat; side: double; transparent: true';
             // entity.setAttributeNode(material);
             break;
-        case "model/gltf+json" :
+        case 'model/gltf+json' :
             entity = document.createElement('a-entity');
-            const gltfmodel = document.createAttribute("gltf-model");
+            const gltfmodel = document.createAttribute('gltf-model');
             gltfmodel.value = body.id;
             entity.setAttributeNode(gltfmodel);
             break;
@@ -139,7 +139,7 @@ function get(url) {
             }
         });
         xhr.onerror = () => {
-            reject(Error("Network Error"));
+            reject(Error('Network Error'));
         };
         xhr.send();
     });
